6c8e96931849c3f7ee6b921e5a4ad1e2ca5c7de9
Xinor-a <zionCode100@gmail.com>
2026-04-06 21:11:04 +0900

    feat: enable to use defaultStyles.js from anywhare

--- scripts/defaultStyles.js
+++ scripts/defaultStyles.js
@@ -1,14 +1,16 @@
+const _scriptBase = new URL(".", document.currentScript.src).href;
+
 const commonStyle = document.createElement("link");
 commonStyle.rel = "stylesheet";
-commonStyle.href = "./styles/common.css";
+commonStyle.href = _scriptBase + "../styles/common.css";
 document.querySelector("head").appendChild(commonStyle);
 
 const sideIndexStyle = document.createElement("link");
 sideIndexStyle.rel = "stylesheet";
-sideIndexStyle.href = "./styles/sideIndex.css";
+sideIndexStyle.href = _scriptBase + "../styles/sideIndex.css";
 document.querySelector("head").appendChild(sideIndexStyle);
 
 const defaultStyle = document.createElement("link");
 defaultStyle.rel = "stylesheet";
-defaultStyle.href = "./styles/toggleStyles.css";
+defaultStyle.href = _scriptBase + "../styles/toggleStyles.css";
 document.querySelector("head").appendChild(defaultStyle);
