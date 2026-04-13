e31fd0c620614971389ff4f340e1d9fc91b93ae7
Xinor-a <zionCode100@gmail.com>
2026-04-13 22:03:02 +0900

    dev: add new way to specify language for code block

--- scripts/defaultStyles.js
+++ scripts/defaultStyles.js
@@ -16,16 +16,24 @@ highlightCss.rel = "stylesheet";
 highlightCss.href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github-dark.min.css";
 document.head.appendChild(highlightCss);
 
-// Load Highlight.js script dynamically
-const highlightScript = document.createElement("script");
-highlightScript.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js";
-highlightScript.onload = () => {
-    // Initialize Highlight.js after the script is loaded
-    hljs.highlightAll();
-};
-document.body.appendChild(highlightScript);
-
 const commonStyle = document.createElement("link");
 commonStyle.rel = "stylesheet";
 commonStyle.href = _scriptBase + "../styles/common.css";
 document.querySelector("head").appendChild(commonStyle);
+
+// Load Highlight.js script dynamically
+const highlightScript = document.createElement("script");
+highlightScript.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js";
+highlightScript.onload = () => {
+    const langSpecifier = /^@lang:([a-z0-9]+)$/;
+    document.querySelectorAll("pre code").forEach((block) => {
+        const lines = block.textContent.split("\n");
+        const match = lines[0].trim().match(langSpecifier);
+        if (match) {
+            block.textContent = lines.slice(1).join("\n");
+            block.classList.add(`language-${match[1]}`);
+            hljs.highlightElement(block);
+        }
+    });
+};
+document.body.appendChild(highlightScript);
